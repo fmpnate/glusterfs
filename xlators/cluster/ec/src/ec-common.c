4e32b9ce87645c83dd025ea89edae9adb7cf77b4
nate.ko <nate.ko@infortrend.com>
2016-08-02 11:24:33 +0800

    cluster/ec: Do not reuse lock when healing is needed.
    Problem:
    In case of lock being reuse by other fop(s), task of update will eventually be
    delivered to the last lock owner. If the lock owner does not update all the
    online bricks due to its mask limitation, difference of version will keep
    growing between good brick and bad brick.
    
    Fix:
    Force update size and version when healing is needed. Do not let the task of
    update be delivered to other fops.

--- xlators/cluster/ec/src/ec-common.c
+++ xlators/cluster/ec/src/ec-common.c
@@ -1638,6 +1638,14 @@ ec_lock_next_owner(ec_lock_link_t *link, ec_cbk_data_t *cbk,
 
     ec_lock_update_good(lock, fop);
 
+    if ((fop->error == 0) && (cbk != NULL) && (cbk->op_ret >= 0)) {
+        if(link->update[0] || link->update[1]){
+            if(ec_fop_needs_heal(link->fop)){
+                lock->release = _gf_true;
+                list_splice_init(&lock->waiting,&lock->frozen);
+            }
+        }
+    }
     lock->exclusive -= (fop->flags & EC_FLAG_LOCK_SHARED) == 0;
     if (list_empty(&lock->owners)) {
         ec_lock_wake_shared(lock, &list);
